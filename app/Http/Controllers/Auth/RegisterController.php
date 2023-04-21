58173f0805289bc4464d1c1a769b40536d1b1806
Nameless0808 <artur19180808@gmail.com>
2023-04-21 10:42:40 +0900

    SNS課題データ追加

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Register Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users as well as their
+    | validation and creation. By default this controller uses a trait to
+    | provide this functionality without requiring any additional code.
+    |
+    */
+
+    use RegistersUsers;
+
+    /**
+     * Where to redirect users after registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    public function register(Request $request){
+        if($request->isMethod('post')){
+
+            $username = $request->input('username');
+            $mail = $request->input('mail');
+            $password = $request->input('password');
+
+            User::create([
+                'username' => $username,
+                'mail' => $mail,
+                'password' => bcrypt($password),
+            ]);
+
+            return redirect('added');
+        }
+        return view('auth.register');
+    }
+
+    public function added(){
+        return view('auth.added');
+    }
+}
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.logout')
+
+@section('content')
+<!-- 適切なURLを入力してください -->
+{!! Form::open(['url' => '/〇〇']) !!}
+
+<p>AtlasSNSへようこそ</p>
+
+{{ Form::label('e-mail') }}
+{{ Form::text('mail',null,['class' => 'input']) }}
+{{ Form::label('password') }}
+{{ Form::password('password',['class' => 'input']) }}
+
+{{ Form::submit('ログイン') }}
+
+<p><a href="/register">新規ユーザーの方はこちら</a></p>
+
+{!! Form::close() !!}
+
+@endsection
